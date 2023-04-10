c62fb72fc3ee2ae3157175b8f7c5284140ca5ac1
OleksandrKambarov <113207904+OleksandrKambarov@users.noreply.github.com>
2023-04-10 17:06:07 +0300

    transactions+

--- src/components/Transactions/Transactions.jsx
+++ src/components/Transactions/Transactions.jsx
@@ -1,34 +1,31 @@
 import './Transactions.module.css';
 import PropTypes from 'prop-types';
+import css from './Transactions.module.css';
 
-export const TransactionHistory = ({ items }) => {
-  return (
-    <table>
-      <thead>
-        <tr>
-          <th>Type</th>
-          <th>Amount</th>
-          <th>Currency</th>
+export const TransactionHistory = ({ items }) => (
+  <table className={css['transaction-history']}>
+    <thead className={css['transaction-head']}>
+      <tr>
+        <th className={css['transaction-title']}>Type</th>
+        <th className={css['transaction-title']}>Amount</th>
+        <th className={css['transaction-title']}>Currency</th>
+      </tr>
+    </thead>
+
+    <tbody>
+      {items.map(item => (
+        <tr className={css['transaction-row']} key={item.id}>
+          <td className={css['transaction-cell']}>{item.type}</td>
+          <td className={css['transaction-cell']}>{item.amount}</td>
+          <td className={css['transaction-cell']}>{item.currency}</td>
         </tr>
-      </thead>
-
-      <tbody>
-        {items.map(({ id, type, amount, currency }) => {
-          return (
-            <tr key={id}>
-              <td>{type}</td>
-              <td>{amount}</td>
-              <td>{currency}</td>
-            </tr>
-          );
-        })}
-      </tbody>
-    </table>
-  );
-};
+      ))}
+    </tbody>
+  </table>
+);
 
 TransactionHistory.propTypes = {
-  friends: PropTypes.arrayOf(
+  items: PropTypes.arrayOf(
     PropTypes.shape({
       id: PropTypes.string.isRequired,
       type: PropTypes.string.isRequired,
