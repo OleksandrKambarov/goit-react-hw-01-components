496afa8119bda64260c4ab42219c719be4a9027b
OleksandrKambarov <113207904+OleksandrKambarov@users.noreply.github.com>
2023-04-10 17:09:29 +0300

    css

--- src/components/Transactions/Transactions.jsx
+++ src/components/Transactions/Transactions.jsx
@@ -3,21 +3,21 @@ import PropTypes from 'prop-types';
 import css from './Transactions.module.css';
 
 export const TransactionHistory = ({ items }) => (
-  <table className={css['transaction-history']}>
-    <thead className={css['transaction-head']}>
+  <table className={css['history']}>
+    <thead className={css['head']}>
       <tr>
-        <th className={css['transaction-title']}>Type</th>
-        <th className={css['transaction-title']}>Amount</th>
-        <th className={css['transaction-title']}>Currency</th>
+        <th className={css['title']}>Type</th>
+        <th className={css['title']}>Amount</th>
+        <th className={css['title']}>Currency</th>
       </tr>
     </thead>
 
     <tbody>
       {items.map(item => (
-        <tr className={css['transaction-row']} key={item.id}>
-          <td className={css['transaction-cell']}>{item.type}</td>
-          <td className={css['transaction-cell']}>{item.amount}</td>
-          <td className={css['transaction-cell']}>{item.currency}</td>
+        <tr className={css['row']} key={item.id}>
+          <td className={css['cell']}>{item.type}</td>
+          <td className={css['cell']}>{item.amount}</td>
+          <td className={css['cell']}>{item.currency}</td>
         </tr>
       ))}
     </tbody>
